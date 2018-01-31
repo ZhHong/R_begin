aa56364424b87cffef80816a126fec9be94ab247
ZhHong <371816216@qq.com>
2018-01-31 18:48:45 +0800

    add mysql example

--- R_begin.R
+++ R_begin.R
@@ -1,7 +1,28 @@
 library(DBI)
 library(RMySQL)
-conn <- dbConnect(MySQL(),dbname="game_development",username="root",password="root",host="127.0.0.1",port=3306)
-dbReadTable(conn,"users")
+db_driver <- dbDriver("MySQL")
+conn <- dbConnect(db_driver,dbname="game_development",username="root",password="root",host="127.0.0.1",port=3306)
+summary(conn)
+dbGetInfo(conn)
+dbListTables(conn)
+dbListFields(conn,"users")
+sql <- "select userid,sex,login_time,platform from users"
+res1 <- dbSendQuery(conn,sql)
+dbFetch(res1,n=10)
+dbListResults(conn)
+#print(res1)
+dbClearResult(res1)
+
+char_set_sql <- "SET NAMES gbk"
+dbSendQuery(conn,char_set_sql)
+
+sql_history <- "select create_time,ip,zip_reason,zip_time,server_type,country,area,city,cost_ingot,cost_gold from rooms_archive"
+res2 <- dbSendQuery(conn,sql_history)
+dbFetch(res2,n=10)
+
+dbListResults(conn)
+dbClearResult(res2)
 
 dbDisconnect(conn)
 
+dbUnloadDriver(db_driver)
--- readme.md
+++ readme.md
@@ -1,5 +1,21 @@
 # 安装RMySQL
 
+* 1 安装mysql包依赖
 ```r
     install.packages("RMySQL")
-``` 
+    ## 查看已安装的包
+    installed.packages()
+```
+* 2 查看R的版本信息
+```r
+    R.version
+```
+* 3 R基本操作
+```r
+    #<-赋值
+    x <- 5
+    #ls()可查看内存中的对象
+    ls()
+    # 向量
+    v <-c(1,2,3)
+```
